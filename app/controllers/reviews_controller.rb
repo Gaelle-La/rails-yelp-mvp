c2d34a9d9fbcca2a5d435ed3693730ddd72e793b
Gaelle Lafargue <gaellelfrgue@gmail.com>
2024-08-07 15:29:58 +0200

    RailsYelpMvp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.new(review_params)
+    @review.save!
+    redirect_to @restaurant
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
